bc8a46d5c45b96223efa0a862f1c68b88d4f23e9
Mikita_Besau <who232doctor@gmail.com>
2023-07-03 15:38:41 +0300

    Version Alpha 8.0

--- Buky.DataAccess/Data/ApplicationDbContext.cs
+++ Buky.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,8 @@ namespace Bulky.DataAccess.Data
 
         public DbSet<ApplicationUser> applicationUsers { get; set; }
 
+        public DbSet<Company> Companies { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
@@ -113,6 +115,39 @@ namespace Bulky.DataAccess.Data
                     Price100 = 20,
                     CategoryId = 1,
                     ImageUrl = string.Empty
+                }
+                );
+
+            modelBuilder.Entity<Company>().HasData(
+                new Company 
+                { 
+                    Id = 1, 
+                    Name = "Tech Solution", 
+                    StreetAddress = "123 Tech St", 
+                    City = "Tech City", 
+                    PostalCode = "12121", 
+                    State = "IL", 
+                    PhoneNumber = "6669990000" 
+                },
+                new Company
+                {
+                    Id = 2,
+                    Name = "Vivid Books",
+                    StreetAddress = "999 Vid St",
+                    City = "Vid City",
+                    PostalCode = "66666",
+                    State = "IL",
+                    PhoneNumber = "7779990000"
+                },
+                new Company
+                {
+                    Id = 3,
+                    Name = "Readers Club",
+                    StreetAddress = "999 Main St",
+                    City = "Lala land",
+                    PostalCode = "99999",
+                    State = "NY",
+                    PhoneNumber = "1113335555"
                 }
                 );
         }
---
+++ BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,94 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAccess.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return View(objCompanyList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            if (id is null || id == 0) //create
+            {
+                return View(new Company());
+            }
+            else //update
+            {
+                Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(companyObj);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company companyObj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (companyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(companyObj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(companyObj);
+                }
+
+                _unitOfWork.Save();
+                TempData["success"] = "Company created successfully";
+                return RedirectToAction("Index", "Company");
+            }
+            else
+            {
+                return View(companyObj);
+            }
+        }
+
+        #region APICALLS
+
+        [HttpGet]
+        public IActionResult GetAll(int id)
+        {
+            var objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = objCompanyList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var companyToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
+            if(companyToBeDeleted is null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(companyToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
+        #endregion
+    }
+}
